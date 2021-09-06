105541efff5a5b56d84bc584e39de9b5c0197dba
KingricharDV#0439 <northerncommunity1@gmail.com>
2021-09-05 15:48:54 -1000

    updating

--- src/chainparamsbase.h
+++ src/chainparamsbase.h
@@ -7,8 +7,8 @@
 #ifndef BITCOIN_CHAINPARAMSBASE_H
 #define BITCOIN_CHAINPARAMSBASE_H
 
-#define __RPCPORT_MAINNET__ 9998
-#define __RPCPORT_TESTNET__ 19998
+#define 20433 9998
+#define TESTNET 51473
 
 #include <string>
 #include <vector>
